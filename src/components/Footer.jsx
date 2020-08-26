3697966820c08af61fa11a9189449aa1bc26c752
Ezra Lazuardy <ezralucio@gmail.com>
2020-08-27 04:21:23 +0700

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,63 +1,43 @@
-import React from "react";
-import { Link } from "gatsby";
-import styled from "@emotion/styled";
-import colors from "styles/colors";
-import Logo from "components/_ui/Logo";
-import spooch from "images/oscar-icon.png"
+import React from "react"
 
-const FooterContainer = styled("div")`
-    padding-top: 3.75em;
-    padding-bottom: 3em;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+// const FooterContainer = styled("div")`
+//     padding-top: 3.75em;
+//     padding-bottom: 3em;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//
+//     svg {
+//         max-width: 50px;
+//     }
+// `
 
-    svg {
-        max-width: 50px;
-    }
-`
-
-const FooterAuthor = styled("a")`
-    font-size: 0.75em;
-    color: ${colors.grey700};
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-decoration: none;
-    margin-top: 1.5em;
-
-     &:hover {
-         color: ${colors.blue900};
-
-        .FooterSpooch {
-            animation-name: rotate;
-            animation-duration: 1.5s;
-            animation-iteration-count: infinite;
-            animation-timing-function: linear;
-        }
-    }
-
-    @keyframes rotate {
-        from {transform: rotate(0deg);}
-        to {transform: rotate(360deg);}
-    }
-`
-
-const FooterSpooch = styled("img")`
-    max-width: 33px;
-    margin-top: 0.25em;
-`
+// const FooterAuthor = styled("a")`
+//     font-size: 0.75em;
+//     color: ${colors.grey700};
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     text-decoration: none;
+//     margin-top: 1.5em;
+//
+//      &:hover {
+//          color: ${colors.blue900};
+//     }
+//
+//     @keyframes rotate {
+//         from {transform: rotate(0deg);}
+//         to {transform: rotate(360deg);}
+//     }
+// `
 
 const Footer = () => (
-    <FooterContainer>
-        <Link to="/">
-            <Logo />
-        </Link>
-        <FooterAuthor href="https://www.marguerite.io/?utm_source=prist&utm_medium=footer&utm_campaign=prist_starter">
-            © 2019 — Designed & developed by Marguerite Roth
-            <FooterSpooch className="FooterSpooch" src={spooch} />
-        </FooterAuthor>
-    </FooterContainer>
+  <></>
+  // <FooterContainer>
+  //     <FooterAuthor to={"/"}>
+  //         © 2020 — Built with 💔 by Ezra Lazuardy
+  //     </FooterAuthor>
+  // </FooterContainer>
 )
 
-export default Footer;
+export default Footer
